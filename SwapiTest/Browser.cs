9e497acd581b1be212fa9bcb2f2188a58de524d9
Kate Chirkina <milightning.ua@gmail.com>
2018-04-26 17:07:05 +0300

    add Browser class

---
+++ SwapiTest/Browser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium;
+
+namespace SwapiTest
+{
+    static class Browser
+    {
+        public static IWebDriver Driver => new ChromeDriver();
+
+
+    }
+}
--- SwapiTest/UnitTest1.cs
+++ SwapiTest/UnitTest1.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
 
 namespace SwapiTest
 {
     [TestClass]
     public class UnitTest1
     {
+        IWebDriver driver;
+
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://swapi.co/");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            driver.Close();
+            driver.Dispose();
+        }
 
         [TestMethod]
         public void TestMethod1()
         {
+            IWebElement textField = driver.FindElement(By.Id("interactive"));
+            var starShipsSearchSearch = "starships";
+            textField.Clear();
+            textField.SendKeys(starShipsSearchSearch);
+              
+
         }
     }
 }
