441ac8bb2f900adfc369ef9de77619161c8c3de1
Kate Chirkina <milightning.ua@gmail.com>
2018-04-27 18:02:02 +0300

    add Application class

---
+++ SwapiTest/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapiTest
+{
+    public class Application
+    {
+        public Browser GetDriver(string url)
+        {
+           
+        }
+    }
+}
