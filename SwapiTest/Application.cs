3b56ed4402939bdab21c881b86874a3d14386a79
Kate Chirkina <milightning.ua@gmail.com>
2018-05-03 17:44:24 +0300

    - add Start and Close methods to Application class

--- SwapiTest/Application.cs
+++ SwapiTest/Application.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium;
+
 
 namespace SwapiTest
 {
     public class Application
     {
-        public Browser GetDriver(string url)
+        public IWebDriver driver = Browser.Driver; 
+        public Application Start(string url)
         {
-           
+            driver.Navigate().GoToUrl(url);
+            return this;
+        }
+
+        public void Close()
+        {
+            driver.Quit();
+            driver.Dispose();
         }
     }
 }
